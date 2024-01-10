f975ea9e8fd8ce43e19634e1aeeb2be5f624da09
Alexander Osipkin <aleksandrosipkin@yandex.ru>
2024-01-10 19:50:03 +0300

    update homework

--- metro_tests_suite/tests/conftest.py
+++ metro_tests_suite/tests/conftest.py
@@ -1,48 +1,36 @@
-import os
-
 import pytest
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import browser
 
-# from utils import attach
-from dotenv import load_dotenv
-
-#
-# @pytest.fixture(scope="session", autouse=True)
-# def load_env():
-#     load_dotenv()
+from metro_tests_suite.utils import attach
 
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
-    # options = Options()
-    # selenoid_capabilities = {
-    #     "browserName": "chrome",
-    #     "browserVersion": "100.0",
-    #     "selenoid:options": {"enableVNC": True, "enableVideo": True},
-    # }
-    # options.capabilities.update(selenoid_capabilities)
-    #
-    # login = os.getenv('LOGIN')
-    # password = os.getenv('PASSWORD')
-    #
-    # driver = webdriver.Remote(
-    #     command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
-    #     options=options,
-    # )
+    options = Options()
+    selenoid_capabilities = {
+        'browserName': 'chrome',
+        'browserVersion': "100.0",
+        'selenoid:options': {'enableVNC': True, 'enableVideo': True},
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f'https://user1:1234@selenoid.autotests.cloud/wd/hub',
+        options=options,
+    )
 
-    # browser.config.driver = driver
+    browser.config.driver = driver
     browser.config.base_url = 'https://online.metro-cc.ru/'
     browser.config.window_height = 1080
     browser.config.window_width = 1920
 
     yield browser
 
-    # attach.add_screenshot(browser)
-    # attach.add_logs(browser)
-    # attach.add_html(browser)
-    # attach.add_video(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
