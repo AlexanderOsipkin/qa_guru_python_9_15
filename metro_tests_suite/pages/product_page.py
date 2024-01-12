e86e7d2da3eea8d9db816327861586e24756c2d4
Alexander Osipkin <aleksandrosipkin@yandex.ru>
2024-01-12 16:39:31 +0300

    fix issues in test

--- metro_tests_suite/pages/product_page.py
+++ metro_tests_suite/pages/product_page.py
@@ -41,6 +41,8 @@ class Product:
         return self
 
     def click_to_the_pickup_button(self):
-        browser.element('[value=pickup]').click()
+        browser.element(
+            '.obtainments-list__item:nth-child(2) .obtainments-list__description'
+        ).click()
         browser.element('[data-qa=button-save-pickup-address]').click()
         return self
